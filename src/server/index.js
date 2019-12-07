451d6bdde66e8a763b9c7577d7297b15834808c6
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-12-07 16:46:21 -0500

    chore: Change backend server port configuration

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const PORT = 8081
 
 const app = express()
 
@@ -13,8 +14,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(port, () => {
+    console.log(`Example app listening on port ${PORT}!`)
 })
 
 app.get('/test', function (req, res) {
