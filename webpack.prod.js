a09eb6596a147c19f8f04ca360c488afb0c5998c
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-12-13 23:24:34 -0500

    chore: Configure webpack for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,31 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    devtool: 'source-map',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(png|jp(e*)g|svg)$/,  
@@ -28,6 +43,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
