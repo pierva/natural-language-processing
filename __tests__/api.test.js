18182e96a8907c10a0106a9872f9409d6f729870
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-12-15 21:07:36 -0500

    test: Review endpoint

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -18,7 +18,7 @@ describe("Check API key and ID configuration", () => {
 
 
 // Test the combined API endpoint - SUCCESS
-describe('Test the combined call - success', () => {
+describe('Test the combined endpoint - success', () => {
     test('should post the data and return json', async (done) => {
         const res = await request(app)
             .post('/process/combined')
@@ -31,15 +31,60 @@ describe('Test the combined call - success', () => {
 })
 
 // Test the combined API endpoint - Validation error
-describe('Test the combined call - empty data', () => {
+describe('Test the combined endpoint - empty data', () => {
     test('should post the data and return error message', async (done) => {
         const res = await request(app)
             .post('/process/combined')
             .send({text: ''})
         expect(res.statusCode).toEqual(400)
-        console.log(res.text)
-        // expect(res.text)
         expect(JSON.parse(res.text).error).toEqual('No text to process.')
+        done()
+    })
+})
+
+// Test the review endpoint - SUCCESS
+describe('Test the review endpoint - success', () => {
+    test('should post the data and return json', async (done) => {
+        const res = await request(app)
+            .post('/process/review')
+            .send({
+                text: 'Some dummy text',
+                domain: 'restaurants'
+            })
+        expect(res.statusCode).toEqual(200)
+        expect(res.text).not.toBe(undefined)
+        expect(JSON.parse(res.text)).toHaveProperty('aspects')
+        done()
+    })
+})
+
+
+// Test the review API endpoint - Validation error (text)
+describe('Test the review endpoint - empty text', () => {
+    test('should post the data and return error message', async (done) => {
+        const res = await request(app)
+            .post('/process/review')
+            .send({
+                    text: '',
+                    domain: 'restaurants'
+                })
+        expect(res.statusCode).toEqual(400)
+        expect(JSON.parse(res.text).error).toEqual('Please enter a review to process.')
+        done()
+    })
+})
+
+// Test the review API endpoint - Validation error (body)
+describe('Test the review endpoint - empty domain', () => {
+    test('should post the data and return error message', async (done) => {
+        const res = await request(app)
+            .post('/process/review')
+            .send({
+                    text: 'Some beatutiful review',
+                    domain: ''
+                })
+        expect(res.statusCode).toEqual(400)
+        expect(JSON.parse(res.text).error).toEqual('Provide a domain')
         done()
     })
 })
