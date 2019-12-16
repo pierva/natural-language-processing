01f8c0e303f997ef401bbdc1d67c432bcfe9a82a
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-12-15 20:57:22 -0500

    test: Combined endpoint validation

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest')
 const app = require('../src/server')
+
 require("regenerator-runtime")
 
 // Test that the Aylien API is reachable
 const dotenv = require('dotenv')
 dotenv.config()
+
 
 // Check that API credentials are configured in dotenv
 describe("Check API key and ID configuration", () => {
@@ -15,13 +17,29 @@ describe("Check API key and ID configuration", () => {
 })
 
 
-// Test the combined API endpoint
-describe('Check combined call', () => {
-    test('should post the data', async (done) => {
+// Test the combined API endpoint - SUCCESS
+describe('Test the combined call - success', () => {
+    test('should post the data and return json', async (done) => {
         const res = await request(app)
             .post('/process/combined')
             .send({text: 'Some dummy text'})
         expect(res.statusCode).toEqual(200)
+        expect(res.text).not.toBe(undefined)
+        expect(JSON.parse(res.text)).toHaveProperty('results')
+        done()
+    })
+})
+
+// Test the combined API endpoint - Validation error
+describe('Test the combined call - empty data', () => {
+    test('should post the data and return error message', async (done) => {
+        const res = await request(app)
+            .post('/process/combined')
+            .send({text: ''})
+        expect(res.statusCode).toEqual(400)
+        console.log(res.text)
+        // expect(res.text)
+        expect(JSON.parse(res.text).error).toEqual('No text to process.')
         done()
     })
 })
