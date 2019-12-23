3b2ce1c26e97e36c07a732cf39e4ed81d4963b84
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-12-23 16:29:09 -0500

    test: API rejection

--- __tests__/form_handler.test.js
+++ __tests__/form_handler.test.js
@@ -6,7 +6,22 @@ const handleSubmit = (endpoint, domain) => {
     return request(endpoint, domain).then(data => data)
 }
 
-it('It should call the API and return data', () => {
+it('should call the API and return data', () => {
     return handleSubmit('review', 'restaurants')
         .then(data => expect(data.domain).toBe('restaurants'));
 });
+
+it('test error with async/await', async () => {
+    expect.assertions(2)
+    try {
+        await handleSubmit('review', 'resorts')
+    } catch (e) {
+        expect(e.error).toEqual('Invalid domain')
+    }
+
+    try {
+        await handleSubmit('invalid', 'hotels')
+    } catch (e) {
+        expect(e.error).toEqual('Invalid endpoint')
+    }
+})
